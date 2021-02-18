74b1de737a975f6c71385279db2b8fd1587d8b6e
Mohamed Selim <dev.m.selim@gmail.com>
2021-02-18 13:03:15 +0300

    Frequency analysis task

---
+++ Frequency analysis/main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+
+        // Geting input
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        String[] words = sc.nextLine().split(" ");
+        sc.close();
+
+        // Definition of variables
+        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<Integer, TreeSet<String>> freqToWords = new HashMap<>();
+
+        // Initialize map
+        for(String w:words)
+            map.put(w, 0);
+
+        // Fill in the frequancies of words
+        for(String w:words)
+            map.put(w, map.get(w)+1);
+        
+        // Initialize freqToWord
+        for(String w:words) {
+            freqToWords.put(map.get(w), new TreeSet<>());     
+        }
+
+        // Associate freq with list of words
+        for(String w:words) {
+            Integer freq = map.get(w);
+            TreeSet<String> wordList = freqToWords.get(freq);
+            wordList.add(w);
+            freqToWords.put(freq, wordList);
+            
+        }
+                
+        // Sort
+        Object[] freq = freqToWords.keySet().toArray();
+        Arrays.sort(freq, Collections.reverseOrder());
+        
+        // Print output
+        for(Object f:freq) {
+            for(String w:freqToWords.get(f)) {
+                System.out.println(w + " " + f);
+            }
+        }
+    }
+}
