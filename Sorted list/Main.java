8ae1e9bab8805a86787749464402e3c45800aedf
Mohamed Selim <dev.m.selim@gmail.com>
2021-02-25 10:05:29 +0300

    test implemented add function

--- Sorted list/Main.java
+++ Sorted list/Main.java
@@ -1,8 +1,23 @@
+/*
+ *  Created by Mohamed Selim
+ *  GitHub: github.com/S3L1M
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-        Node n = new Node(1);
-        n.linkNext(new Node(2));
-        System.out.println(n.hasNext()? n.getNext():"None");
+        SortedList l = new SortedList();
+        
+        Integer[] x = {5, 4, 9, 4, 0, 4, 2, 1, 5, 6};
+        for(Integer a : x) {
+            System.out.println(a);
+            l.add(a);
+            System.out.println(l);
+            System.out.println("Size: " + l.size());
+            System.out.println("Head: " + l.head);
+            System.out.println("Mid: " + l.mid);
+            System.out.println("Tail: " + l.tail);
+        }
+        
     }
 }
--- Sorted list/SortedList.java
+++ Sorted list/SortedList.java
@@ -1,12 +1,152 @@
 class SortedList implements SortedListInterface {  // Sorted in accending order
+    final byte BEFORE_HEAD=0x01, MID_TO_HEAD=0x21, MID_TO_TAIL=0x23, AFTER_TAIL=0x34;
+
+    // TODO set access modifiers of vars and funcs
     Node head=null, mid=null, tail=null;
+    float midMovement = 0;
     private int size = 0;
 
     public SortedList() {}
+    public SortedList(Comparable[] array) {
+        for(Comparable c : array)
+            add(c);
+    }    
+
+
+    private void push_back(Node node) {
+        tail.linkNext(node);
+        node.linkPrevious(tail);
+        tail = node;
+        midMovement += 0.5;
+        size += 1;
+        updateMidPos();
+    }
+
+    private void push_front(Node node) {
+        node.linkNext(head);
+        head.linkPrevious(node);
+        head = node;
+        midMovement -= 0.5;
+        size += 1;
+        updateMidPos();
+    }
+
+    private void insertBefore(Node node, Node beforeNode) {
+        System.out.println("insertBefore "+ node.value);
+        node.link(beforeNode.getPrevious(), beforeNode);
+        beforeNode.linkPrevious(node);
+        midMovement = (node.value.compareTo(mid.value) > 0)? midMovement+0.5f : midMovement-0.5f;
+        size += 1;
+        updateMidPos();
+    }
+    
+    private void insertAfter(Node node, Node afterNode) {
+        System.out.println("insertAfter "+ node.value);
+        node.link(afterNode, afterNode.getNext());
+        afterNode.linkNext(node);
+        midMovement = (node.value.compareTo(mid.value) > 0)? midMovement+0.5f : midMovement-0.5f;
+        size += 1;
+        updateMidPos();
+    }
+
+    private void updateMidPos() {
+        if(midMovement == 1.0f) {
+            System.out.println("Going to next");
+            mid = mid.getNext();
+            midMovement = 0;
+        }
+        else if(midMovement == -1.0f) {
+            System.out.println("Going to previous");
+            mid = mid.getPrevious();
+            midMovement = 0;
+        }
+    }
+
+    private byte findRange(Comparable value) {
+        // TODO add else case to insert near head/tail
+        if(value.compareTo(head.value) < 0) return BEFORE_HEAD;
+        if(value.compareTo(tail.value) > 0) return AFTER_TAIL;
+        if(value.compareTo(mid.value) < 0)
+            return MID_TO_HEAD;
+        else
+            return MID_TO_TAIL;
+    }
 
     @Override
     public void add(Comparable item) {
-        
+        Node entry = new Node(item);
+        switch(size) {
+            case 0:
+                System.out.println("Case 0");
+                head = mid = tail = entry;
+                size = 1;
+                break;
+
+            case 1:
+                System.out.println("Case 1");
+                if(item.compareTo(head.value) >= 0) {
+                    tail.linkNext(entry);
+                    entry.linkPrevious(tail);
+                    tail = entry;
+                } else {
+                    entry.linkNext(head);
+                    head.linkPrevious(entry);
+                    head = entry;
+                }
+                mid = head;
+                size = 2;
+                break;
+
+            case 2:
+                System.out.println("Case 2");
+                if(item.compareTo(tail.value) >= 0) {
+                    tail.linkNext(entry);
+                    entry.linkPrevious(tail);
+                    mid = tail;
+                    tail = entry;
+                }
+                else if(item.compareTo(head.value) <= 0) {
+                    entry.linkNext(head);
+                    head.linkPrevious(entry);
+                    mid = head;
+                    head = entry;
+                } else {
+                    mid = entry;
+                    mid.link(head, tail);
+                }
+                size = 3;
+                break;
+                
+            default:
+                System.out.println("Default");
+                switch(findRange(item)) {
+                    case MID_TO_HEAD:
+                        System.out.println("Mid to Head");
+                        Node ptr = mid;
+                        while(ptr.hasPrevious()) {
+                            if(item.compareTo(ptr.value) >= 0)
+                                insertAfter(entry, ptr);
+                            ptr = ptr.getPrevious();
+                        }
+                        break;
+                    case MID_TO_TAIL:
+                        System.out.println("Mid to tail");
+                        ptr = mid;
+                        while(ptr.hasNext()) {
+                            if(item.compareTo(ptr.value) <= 0)
+                                insertBefore(entry, ptr);
+                            ptr = ptr.getNext();
+                        }
+                        break;
+                    case AFTER_TAIL:
+                        System.out.println("After tail");
+                        push_back(entry);
+                        break;
+                    case BEFORE_HEAD:
+                        System.out.println("Before head");
+                        push_front(entry);
+                }
+        }
     }
 
     @Override
@@ -55,13 +195,14 @@ class SortedList implements SortedListInterface {  // Sorted in accending order
 
     @Override
     public String toString() {
-        String s = "[";
+        if(head==null) return "[]";
+        StringBuilder s = new StringBuilder("[");
         Node ptr = head;
         while(ptr.hasNext()) {
-            s = s.concat(ptr.toString().concat(", "));
+            s.append(ptr.toString()).append(", ");
             ptr = ptr.getNext();
         }
-        s = s.concat(tail.toString().concat("]"));
-        return s;
+        s.append(tail.toString()).append("]");
+        return s.toString();
     }
 }
